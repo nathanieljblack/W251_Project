3749b8fb8659f65d37bec07572fe3f31e78c828e
Malini Mittal <malini.mittal@gmail.com>
2015-06-29 23:57:27 -0500

    updates

--- preprocessor/resize_spark_mapper.py
+++ preprocessor/resize_spark_mapper.py
@@ -5,6 +5,8 @@ import os
 from pyspark import SparkContext
 from PIL import Image
 import numpy as np
+import glob
+import cPickle
 
 def make_square(im):
     new_h = min(im.shape[0], im.shape[1])
@@ -34,39 +36,50 @@ def resize_image(im, img_size):
     img = Image.fromarray(im)
     return img.resize((img_size, img_size))
 
-out_dir = '/root/W251_Project/preprocessor/preprocessed'
-try:
-    os.mkdir(out_dir)
-except OSError, e:
-    pass 
-
+CUR_DIR = '/root/W251_Project/preprocessor/'
 IMAGE_SIZE = 256
+TEST_OR_TRAIN = 'train'
+
+def make_img_array(im_path):
+    return np.array(Image.open(im_path))
 
 def resize_image_file(file):
     file = file.strip()
-    im = Image.open(file)
-    imarr = np.array(im)
+    imarr = make_img_array(file)
     sq_im = make_square(imarr)
     cr_im = crop(sq_im)
     re_im = resize_image(cr_im, IMAGE_SIZE)
-    outfile = out_dir + '/' + os.path.basename(file)
+    outfile = CUR_DIR + TEST_OR_TRAIN + '_' + str(IMAGE_SIZE) + '/' + os.path.basename(file)
     re_im.save(outfile, 'JPEG')
-    print "writing", outfile
     return 1
+
+def save_images(dir):
+    img_paths = glob.glob(dir + "/*.jpeg")
+    img_list = map(make_img_array, img_paths)
+    pfile = CUR_DIR + TEST_OR_TRAIN + '_' + str(IMAGE_SIZE) + '.cpickle' 
+    f = open(pfile, 'wb')
+    cPickle.dump(img_list, f)
 
 def main(image_files):
     sc = SparkContext( appName="Resize Images")
     sc.parallelize(image_files).map(resize_image_file).count()
 
+    #read all the resized images into an array to save as a pickled object
+    out_dir = CUR_DIR + TEST_OR_TRAIN + '_' + str(IMAGE_SIZE)
+    save_images(out_dir)
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "The program needs a filename which lists the image files"
+        print 'Usage: $SPARK_HOME/bin/spark-submit resize_spark_mapper.py <test_or_train> <image_size> <directory_with_image_files>'
         exit()
 
-    file = sys.argv[1]
-    images = []
-    with open(file) as f:
-        for line in f:
-            images.append(line.strip())
+    TEST_OR_TRAIN = sys.argv[1]
+    IMAGE_SIZE = int(sys.argv[2])
+    img_dir = sys.argv[3]
+    images = glob.glob(img_dir + "/*.jpeg")
+    try:
+        out_dir = CUR_DIR + TEST_OR_TRAIN + '_' + str(IMAGE_SIZE)
+        os.mkdir(out_dir)
+    except OSError, e:
+        pass
     main(images)
-
