4972d10e1c246d4e3f56b0172935bc6a108396c6
Malini Mittal <malini.mittal@ischool.berkeley.edu>
2015-07-27 00:17:36 -0700

    updates to create a csv

--- preprocessor/resize_mapper.py
+++ preprocessor/resize_mapper.py
@@ -4,6 +4,8 @@ import sys
 import os
 from PIL import Image
 import numpy as np
+import glob
+import csv
 
 def make_square(im):
     new_h = min(im.shape[0], im.shape[1])
@@ -32,15 +34,43 @@ def resize_image(im, img_size):
     img = Image.fromarray(im)
     return img.resize((img_size, img_size))
 
+def make_img_arr(im):
+    return np.array(im).ravel().tolist()
+
+def add_to_csv(arr, label=None):
+    f = csvfile_p
+    if label != None:
+        arr = label + arr
+    for i, j in enumerate(arr):
+        if i == (len(arr) - 1):
+            f.write(str(j) + "\n")
+        else:
+            f.write(str(j) + ",")
+
 out_dir = 'preprocessed'
 try:
     os.mkdir(out_dir)
 except OSError, e:
     pass
 
-IMAGE_SIZE = 256
+TEST_OR_TRAIN = sys.argv[1]
+IMAGE_SIZE = int(sys.argv[2])
+img_dir = sys.argv[3]
+images = glob.glob(img_dir + "/*.jpeg")
 
-for file in sys.stdin:
+csvfile = TEST_OR_TRAIN + '_' + str(IMAGE_SIZE) + '.csv'
+csvfile_p = open(csvfile, 'w')
+
+d = {}
+if TEST_OR_TRAIN == 'train':
+    #Make image mapping
+    reader = csv.reader(open('trainLabels.csv', 'r'))
+    #Make dictionary from each row in the CSV (skip first row)
+    for k,v in reader:
+        if k != "image":
+            d[k] = int(v)
+
+for file in images:
     file = file.strip()
     im = Image.open(file)
     imarr = np.array(im)
@@ -49,3 +79,10 @@ for file in sys.stdin:
     re_im = resize_image(cr_im, IMAGE_SIZE)
     outfile = out_dir + '/' + os.path.basename(file)
     re_im.save(outfile, 'JPEG')
+    img_arr = make_img_arr(re_im)
+    img_label = None
+    if TEST_OR_TRAIN == 'train':
+        img_label = [d[os.path.basename(file)[:-5]]]
+    add_to_csv(img_arr, img_label)
+
+csvfile_p.close()
